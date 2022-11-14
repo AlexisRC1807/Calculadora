e22ccb2907c67cb4250b958f7c0a9035b96a3485
Alexis <118144828+AlexisRC1807@users.noreply.github.com>
2022-11-13 20:03:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/*Codigo de calculadora*/
+
+
+function numeros(valor){
+    document.getElementById('Resultado').value += valor;
+
+}
+
+function operacion(){
+    var opera = document.getElementById('Resultado').value;
+    if( opera == 0){
+        document.getElementById('Resultado'.value = "Sin operacion");
+
+    } else{
+        document.getElementById('Resultado').value = eval(opera);
+    }
+
+}
+
+function resetear(){
+    document.getElementById("Resultado").value = " ";
+}
